b0a95d830398dd13800b828626a275ef161e5243
lugutierrezro <lugutierrezro@ucvvirtual.edu.pe>
2025-06-29 21:47:34 -0500

    Add files via upload

--- api_estado.php
+++ api_estado.php
@@ -4,6 +4,11 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require 'Conexion.php';
 error_reporting(0);
 ini_set('display_errors', 0);
